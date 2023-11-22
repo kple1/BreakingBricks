725590247c3be225d131560db93a5618322279f7
Leeple <kple1@naver.com>
2023-11-22 20:10:15 +0900

    벽 구조, 함수 분할

--- main.c
+++ main.c
@@ -1,21 +1,40 @@
 #include<stdio.h>
-#include <stdlib.h>
-#include <wchar.h>
-#include <locale.h>
+#include <windows.h>
+
+void GotoXY(int x, int y);
+void wall();
 
 int main() {
-    char main[11][6] = {};
-
+    GotoXY(32, 5);
+    wall();
+    //대기
+    int a;
+    scanf("%d", a);
+    return 0;
+}
+void wall() {
+    int main[22][12] = {};
     int count = 0;
-    for (int i = 0; i < 11; i++) {
-        for (int j = 0; j < 6; j++) {
+
+    for (int i = 0; i < 22; i++) {
+        for (int j = 0; j < 12; j++) {
             main[i][j] = count;
             count++;
-            if (main[i][j] % 6 == 0 || (main[i][j] + 1) % 6 == 0) printf("%s ", "\u25A1");
-            else if (main[i][j] == j || main[i][j] == j + 60) printf("%s ", "\u25A1");
-            else main[i][j] = printf("%s ", " ");
+            if (main[i][j] % 12 == 0 || (main[i][j] + 1) % 12 == 0) {
+                printf("%s ", "\u25A1");
+            } else if (main[i][j] == j || main[i][j] == j + 252)  {
+                printf("%s ", "\u25A1");
+            } else {
+                main[i][j] = printf("%s ", " ");
+            }
         }
         printf("\n");
     }
-    return 0;
+}
+
+void GotoXY(int x, int y) {
+    COORD Pos;
+    Pos.X = x;
+    Pos.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
