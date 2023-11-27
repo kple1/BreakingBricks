cdcc35ff8e5afcf5a96d39ec8f5e848eb5e6357d
Leeple <kple1@naver.com>
2023-11-27 16:40:41 +0900

    벽 구조, 함수 분할

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
 #include <windows.h>
+#include <conio.h>
 
-void GotoXY(int x, int y);
+void GotoXY(double x, double y);
 void wall();
+void setBall();
+void changeDirection();
 
 int main() {
-    GotoXY(32, 5);
-    wall();
+    wall(); //벽 설정
+    setBall(); //공 설정
+    changeDirection(); //공을 발사할 위치 설정 (keyEvent 발생)
     //대기
     int a;
     scanf("%d", a);
@@ -32,7 +36,34 @@ void wall() {
     }
 }
 
-void GotoXY(int x, int y) {
+void setBall() {
+    GotoXY(10, 20);
+    printf("\u25CF");
+}
+
+int direction = 3; //중간 값 3
+void changeDirection() {
+    GotoXY(10, 19);
+    printf("\u2191");
+
+    int site = getchar();
+    if (site == 224) {
+        site = getch();
+
+        switch (site) {
+            case 75:
+                if () {}
+                break;
+            case 77:
+                if () {}
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+void GotoXY(double x, double y) {
     COORD Pos;
     Pos.X = x;
     Pos.Y = y;
