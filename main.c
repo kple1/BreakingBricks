6fb13f163bcd32a233c865f2a2a78d2b8035b27c
Leeple <kple1@naver.com>
2023-11-30 20:05:31 +0900

    방향 조절, 코드 정리, 공 발사

--- main.c
+++ main.c
@@ -5,54 +5,145 @@
 #define LEFT 75
 #define RIGHT 77
 
+#define bottom 1
+#define leftWall 60
+#define RightWall 95
+#define floor 31
+
 void GotoXY(int x, int y);
-void keyListener(char buffer[]);
+void shoot();
+void changeAngle_Top();
+void changeAngle_Left();
+void changeAngle_Right();
+void keyListener(); //break 대체 할 system 만들어지면 함수 사용
+void systemSetting();
+void showTitle();
+void reset();
+
+int floorX = 77;
+int floorY = 31;
+
+int ballX = 80;
+int ballY = 30;
+
+int changeDirection = 0;
+int startData = 78;
 
 int main() {
-    char buf[1000];
+    systemSetting();
+    showTitle();
 
-    FILE *fpr = fopen("moveFloor\\mainFloor", "r");
-
-    int lineCount = 0;
-    while (!feof(fpr)) {
-        fgets(buf, 1000, fpr);
-        printf("%s", buf);
-        lineCount++;
-        if (lineCount - 1 == 33) {
-            lineCount -= 1;
-            keyListener(buf);
-        }
-    }
-    fclose(fpr);
-    return 0;
-}
-
-void keyListener(char buffer[]) {
     char c;
-    int x = 78;
-    int y = 27;
-
     while (1) {
         if (_kbhit()) {
             c = _getch();
             if (c == -32) {
                 c = _getch();
-                if (x == 60 || x == 101) break;
-
+                if (floorX == 60 || floorX == 95) break; //발판이 벽에 닿으면
                 switch (c) {
                     case LEFT:
-                        GotoXY(x - 1, y); printf("%s", buffer);
-                        x -= 1;
+                        GotoXY(floorX, floorY);
+                        printf("      ");
+                        floorX -= 1;
+                        GotoXY(floorX, floorY);
+                        printf("______");
                         break;
                     case RIGHT:
-                        GotoXY(x + 1, y); printf("%s", buffer);
-                        x += 1;
+                        GotoXY(floorX, floorY);
+                        printf("      ");
+                        floorX += 1;
+                        GotoXY(floorX, floorY);
+                        printf("______");
                         break;
                 }
             }
         }
+
+        /**
+         * 첫 시작이 N ~ 78 , 76 ~ 0 만약 77일시 바로 공이 발사됨을 방지하기 위하여, 변수로 따로 지정.
+         * 만약 시작하면 움직여도 멈추지 않게 모든 범위 할당 /
+         * (최대 창 기준) 창 바뀌면 좌표도 달라짐.
+         */
+        if (floorX >= startData || floorX <= 76) {
+            if (startData == 78) startData -= 1;
+            if (ballY == bottom) changeDirection = 1;
+            if (ballX == leftWall) changeDirection = 2;
+            if (ballX == RightWall) changeDirection = 3;
+            if (ballY == floor) changeDirection = 4;
+            if (changeDirection == 0) {
+                shoot();
+            } else if (changeDirection == 1) {
+                changeAngle_Top();
+            } else if (changeDirection == 2) {
+                changeAngle_Left();
+            } else if (changeDirection == 3) {
+                changeAngle_Right();
+            } else if (changeDirection == 4) {
+                reset();
+            }
+        }
     }
+    return 0;
 }
+
+void reset() {
+    GotoXY(80, 30);
+    printf("○");
+    changeDirection = 0;
+    startData = 78;
+}
+
+void showTitle() {
+    char buf[100];
+    FILE *fpr = fopen("wall", "r");
+
+    while (!feof(fpr)) {
+        fgets(buf, 100, fpr);
+        printf("%s", buf);
+    }
+    fclose(fpr);
+}
+
+void shoot() {
+    GotoXY(80, --ballY);
+    printf("○");
+    Sleep(50); //공이 빨리 사라지지 않게 하기 위함
+    GotoXY(80, ballY);
+    printf(" ");
+}
+
+void changeAngle_Top() {
+    GotoXY(--ballX, ++ballY);
+    printf("○");
+    Sleep(50);
+    GotoXY(ballX, ballY);
+    printf(" ");
+}
+
+void changeAngle_Left() {
+    GotoXY(++ballX, ++ballY);
+    printf("○");
+    Sleep(50);
+    GotoXY(ballX, ballY);
+    printf(" ");
+}
+
+void changeAngle_Right() {
+    GotoXY(--ballX, ++ballY);
+    printf("○");
+    Sleep(50);
+    GotoXY(ballX, ballY);
+    printf(" ");
+}
+
+void systemSetting() {
+    CONSOLE_CURSOR_INFO cursorInfo = {0,};
+    cursorInfo.dwSize = 1;
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+}
+
+void keyListener() {}
 
 void GotoXY(int x, int y) {
     COORD Pos;
