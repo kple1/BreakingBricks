f8a738e1691a2925ae6cbebf0fd631076c7800cf
Leeple <kple1@naver.com>
2023-11-30 23:53:59 +0900

    방향 조절, 코드 정리, 공 발사, 프로젝트 이름변경, define수정

---
+++ cmake-build-debug/CMakeFiles/BreakingBricks.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BreakingBricks.exe"
+  "BreakingBricks.exe.manifest"
+  "BreakingBricks.pdb"
+  "CMakeFiles/BreakingBricks.dir/main.c.obj"
+  "CMakeFiles/BreakingBricks.dir/main.c.obj.d"
+  "libBreakingBricks.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BreakingBricks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BreakingBricks.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for BreakingBricks.
+# This may be replaced when dependencies are built.
