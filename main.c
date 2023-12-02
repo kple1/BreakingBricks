19d51cc382cb346d0e213009491e8529cf38bb5e
Leeple <kple1@naver.com>
2023-12-02 20:39:50 +0900

    90% 각도 조절 시스템 완성

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 
 #define top 1
 #define leftWall 60
-#define RightWall 95
+#define RightWall 99
 #define floor 31
 
 void GotoXY(int x, int y);
@@ -15,7 +15,9 @@ void shoot();
 void changeAngle_Top();
 void changeAngle_Left();
 void changeAngle_Right();
-void keyListener(); //break 대체 할 system 만들어지면 함수 사용
+void changeAngle_LeftTop();
+void changeAngle_RightTop();
+void straight();
 void systemSetting();
 void showTitle();
 void reset();
@@ -28,9 +30,13 @@ int ballY = 30;
 
 int changeDirection = 0;
 int startData = 78;
+int ballStartData = 0;
 
 int main() {
     systemSetting();
+    //첫 시작 파일 깨짐을 위해 불러온 뒤 클리어 후 다시 불러옴
+    showTitle();
+    system("cls");
     showTitle();
 
     char c;
@@ -39,7 +45,13 @@ int main() {
             c = _getch();
             if (c == -32) {
                 c = _getch();
-                if (floorX == 60 || floorX == 95) break; //발판이 벽에 닿으면
+                //발판이 벽에 닿으면
+                if (floorX == 60) {
+                    floorX = 61;
+                } else if (floorX == 95) {
+                    floorX = 94;
+                }
+                //keyListener
                 switch (c) {
                     case LEFT:
                         GotoXY(floorX, floorY);
@@ -66,20 +78,56 @@ int main() {
          */
         if (floorX >= startData || floorX <= 76) {
             if (startData == 78) startData -= 1;
-            if (ballY == top) changeDirection = 1;
-            if (ballX == leftWall) changeDirection = 2;
-            if (ballX == RightWall) changeDirection = 3;
-            if (ballY == floor) changeDirection = 4;
-            if (changeDirection == 0) {
-                shoot();
-            } else if (changeDirection == 1) {
-                changeAngle_Top();
-            } else if (changeDirection == 2) {
-                changeAngle_Left();
-            } else if (changeDirection == 3) {
-                changeAngle_Right();
-            } else if (changeDirection == 4) {
-                reset();
+            if (ballStartData == 0) {
+                if (ballY == top) changeDirection = 1;
+                if (ballX == leftWall) changeDirection = 2;
+                if (ballX == RightWall) changeDirection = 3;
+                if (ballY == floor) changeDirection = 4;
+            } else {
+                if (ballY == top) changeDirection = 1;
+                if (ballX == leftWall) changeDirection = 5;
+                if (ballX == RightWall) changeDirection = 7;
+                if (ballY == floor) changeDirection = 4;
+            }
+
+            if (ballX == floorX && ballY == floorY || ballX == floorX + 1 && ballY == floorY) changeDirection = 7; //왼쪽 2개
+            if (ballX == floorX + 2 && ballY == floorY || ballX == floorX + 3 && ballY == floorY) changeDirection = 6; //가운데 2개
+            if (ballX == floorX + 4 && ballY == floorY || ballX == floorX + 5 && ballY == floorY) changeDirection = 5; //오른쪽 2개
+
+            switch (changeDirection) {
+                case 0:
+                    shoot();
+                    ballStartData = 0;
+                    break;
+                case 1:
+                    changeAngle_Top();
+                    ballStartData = 0;
+                    break;
+                case 2:
+                    changeAngle_Left();
+                    ballStartData = 0;
+                    break;
+                case 3:
+                    changeAngle_Right();
+                    ballStartData = 0;
+                    break;
+                case 4:
+                    reset();
+                    break;
+                case 5:
+                    changeAngle_RightTop();
+                    ballStartData = 1;
+                    break;
+                case 6:
+                    straight();
+                    ballStartData = 0;
+                    break;
+                case 7:
+                    changeAngle_LeftTop();
+                    ballStartData = 1;
+                    break;
+                default:
+                    break;
             }
         }
     }
@@ -87,10 +135,18 @@ int main() {
 }
 
 void reset() {
-    GotoXY(80, 30);
-    printf("○");
+    floorX = 77;
+    floorY = 31;
+
+    ballX = 80;
+    ballY = 30;
+
     changeDirection = 0;
     startData = 78;
+    ballStartData = 0;
+
+    system("cls");
+    showTitle();
 }
 
 void showTitle() {
@@ -109,6 +165,14 @@ void shoot() {
     printf("○");
     Sleep(50); //공이 빨리 사라지지 않게 하기 위함
     GotoXY(80, ballY);
+    printf(" ");
+}
+
+void straight() {
+    GotoXY(ballX, --ballY);
+    printf("○");
+    Sleep(50);
+    GotoXY(ballX, ballY);
     printf(" ");
 }
 
@@ -136,14 +200,28 @@ void changeAngle_Right() {
     printf(" ");
 }
 
+void changeAngle_LeftTop() {
+    GotoXY(--ballX, --ballY);
+    printf("○");
+    Sleep(50);
+    GotoXY(ballX, ballY);
+    printf(" ");
+}
+
+void changeAngle_RightTop() {
+    GotoXY(++ballX, --ballY);
+    printf("○");
+    Sleep(50);
+    GotoXY(ballX, ballY);
+    printf(" ");
+}
+
 void systemSetting() {
     CONSOLE_CURSOR_INFO cursorInfo = {0,};
     cursorInfo.dwSize = 1;
     cursorInfo.bVisible = FALSE;
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
 }
-
-void keyListener() {}
 
 void GotoXY(int x, int y) {
     COORD Pos;
